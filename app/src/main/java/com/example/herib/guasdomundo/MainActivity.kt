1cb77cb94217449119954d9bd7f636b6245c3238
heribertofilho <heriberto.filho@outlook.com>
2017-06-02 12:49:55 -0300

    Versão revisada!

---
+++ app/src/main/java/com/example/herib/guasdomundo/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.herib.guasdomundo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.maps_toolbar.*
+
+class MainActivity : AppCompatActivity() {
+    private var mapsFragment: Fragment? = null
+    private var mSelectedItem: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener@ {
+            menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    if (mapsFragment == null)
+                        mapsFragment = MapsFragment()
+                    setFragment(menuItem, mapsFragment!!)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    updateToolbar(1)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+        navigation.selectedItemId = R.id.navigation_home
+    }
+
+    private fun setFragment(item: MenuItem, fragment: Fragment) {
+        mSelectedItem = item.itemId
+        val menuSize: Int = navigation!!.menu.size() - 1
+        for (i in 0..menuSize) {
+            val menuItem = navigation!!.menu.getItem(i)
+            menuItem.isChecked = (menuItem.itemId == item.itemId)
+        }
+
+        val ft = supportFragmentManager.beginTransaction()
+        if (supportFragmentManager.findFragmentById(fragment.id) == null)
+            ft.add(R.id.container, fragment, fragment.tag)
+        else
+            ft.show(fragment)
+        ft.commit()
+
+        updateToolbar(fragment.id)
+    }
+
+    private fun updateToolbar(id: Int) {
+        when (id) {
+            mapsFragment!!.id -> {
+                toolbar.visibility = GONE
+                toolbarMaps.visibility = VISIBLE
+            }
+            else -> {
+                toolbar.visibility = VISIBLE
+                toolbarMaps.visibility = GONE
+            }
+        }
+    }
+
+}
